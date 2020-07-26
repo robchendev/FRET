71a1140e02090da89160d61eb66ee34e86c06acc
chendumpling99 <66714443+chendumpling99@users.noreply.github.com>
2020-07-26 04:32:59 -0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -10,11 +10,10 @@ module.exports = {
 	.setTitle('Commands')
     .addField(`\`${prefix}thanks <user>\``, 'Thanks a single user and gives them points', false)
     .addField(`\`${prefix}thanks <user1> <user2> <user3>\``, 'Thanks multiple users and gives them points', false)
-    .addField(`\`${prefix}rankup\``, 'Ranks you up if you have enough points. Otherwise, shows how many points you need.', false)
+    .addField(`\`${prefix}rankup\``, 'Ranks you up if you have enough points. Otherwise, shows how many points you need to rank up.', false)
     .addField(`\`${prefix}points\``, 'Shows how many points you have.', false)
-    .addField(`\`${prefix}points <user>\``, 'Shows another user\'s points and amount needed for the next rank', false)
-    .addField(`\`${prefix}report <user> reason\``, 'Reports a user to the mods for abusing the -thanks command', false)
-    .addField(`\`${prefix}about\``, 'Shows useless info about this bot (This bot should be renamed \'Gratitude Bot\'', false);
+    .addField(`\`${prefix}points <user>\``, 'Shows a user\'s points and amount needed for the next rank', false)
+    .addField(`\`${prefix}about\``, 'Shows useless info about this bot', false);
 
     msg.channel.send(embedMsg);
 
