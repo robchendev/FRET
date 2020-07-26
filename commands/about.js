477eaa27a8ba7cbc853e9d05260ea27ed5003a7d
chendumpling99 <66714443+chendumpling99@users.noreply.github.com>
2020-07-26 04:36:40 -0700

    Update about.js

--- commands/about.js
+++ commands/about.js
@@ -11,6 +11,7 @@ module.exports = {
         .setURL('https://github.com/chendumpling99/ThanksBot')
         .setAuthor('About ThanksBot', '', '')
         .setDescription('ThanksBot is a simple Javascript Discord bot that stores user-awarded points in a database (specifically Mongoose). The idea is to encourage users to ask and answer questions. With each question answered, other users can choose to thank the user who answered the question by giving them points, leveling them up through roles.')
+        .addField('How points are calculated', 'The expression used is 100/(users^0.5)', false)
         .setFooter('Developed by Robert Chen', '');
 
         msg.channel.send(embedMsg);
