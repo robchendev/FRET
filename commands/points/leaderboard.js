ce6877fbadedc307ce4715d184ed4108bfd9bb51
chendumpling99 <66714443+chendumpling99@users.noreply.github.com>
2022-01-27 13:17:32 -0800

    fixed 0 points bug and added an option to specify number of rows

--- commands/points/leaderboard.js
+++ commands/points/leaderboard.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const mongoose = require("mongoose");
+const { arrayBuffer } = require("stream/consumers");
 const configHandler = require(`../../handlers/configurationHandler.js`);
 configHandler.initialize();
 const pointsList = require("../../models/addPoints.js");
@@ -19,10 +20,10 @@ mongoose.connect(configHandler.secrets.Mongo, {
  * @param {Client} bot - the client that allows use of discord js
  * @param {Message} msg - the original message sent by the command 
  */
-function generateLeaderboard(bot, msg){
+function generateLeaderboard(bot, msg, maxRows){
+
     pointsList.find({}, (err, documents) => {
         if (err) console.log(err);
-
         sortedArray = [];
         let name = "";
         let userPlacement = undefined;
@@ -56,8 +57,13 @@ function generateLeaderboard(bot, msg){
             return b[0]-a[0];
         })
         
+        // make sure maxRows isnt bigger than array's size
+        if (maxRows > sortedArray.length) {
+            maxRows = sortedArray.length;
+        }
+
         // get column minimum widths
-        for (let i = 0; i < 11; i++){
+        for (let i = 0; i < maxRows; i++){
             if (sortedArray[i][1].length > nameSpacing)
                 nameSpacing = sortedArray[i][1].length;
             if (sortedArray[i][0].toString().length > placementSpacing)
@@ -71,24 +77,36 @@ function generateLeaderboard(bot, msg){
         let description = `\`\`\`py\n`;
 
         // loop through and add rows based on array up to 10
-        for (let i = 0; i < 10; i++) {
-            description += `${i+1} ${" ".repeat(placementSpacing - (i+1).toString().length)}`;
-            description += `${sortedArray[i][1]} ${" ".repeat(nameSpacing - sortedArray[i][1].length)}`;
-            description += `${sortedArray[i][0]}`;
-            description += `\n`;
+        for (let i = 0; i < sortedArray.length; i++) {
 
+            // ensures the table does not print more than 10 rows
+            if (i < maxRows){
+                description += `${i+1} ${" ".repeat(placementSpacing - (i+1).toString().length)}`;
+                description += `${sortedArray[i][1]} ${" ".repeat(nameSpacing - sortedArray[i][1].length)}`;
+                description += `${sortedArray[i][0]}`;
+                description += `\n`;
+            }
             // store the command's user's stats
             if (sortedArray[i][2] === msg.author.id){
                 userPlacement = i+1;
                 userPoints = sortedArray[i][0];
             }
         }
+
+        // prints dividing line to show the command's user's stats
         let embedWidth = nameSpacing + columnPaddingRight + placementSpacing + columnPaddingRight + pointLength;
-        // show the command's user's stats
-        description += `${"-".repeat(embedWidth)}\nYour server stats:\n`
-        description += `${userPlacement} ${" ".repeat(placementSpacing - userPlacement.toString().length)}`;
-        description += `${msg.author.username} ${" ".repeat(nameSpacing - msg.author.username.length)}`;
-        description += `${userPoints}`;
+        description += `${"-".repeat(embedWidth)}\nYour server stats:\n`;
+
+        // this will only run when command's user has a pointdata schema
+        if (userPlacement != undefined){
+            // show the command's users stats
+            
+            description += `${userPlacement} ${" ".repeat(placementSpacing - userPlacement.toString().length)}`;
+            description += `${msg.author.username} ${" ".repeat(nameSpacing - msg.author.username.length)}`;
+            description += `${userPoints}`;
+        } else {
+            description += `You have 0 points`;
+        }
         description += `\n\`\`\``;
 
         const embedMsg = new Discord.MessageEmbed()
@@ -102,7 +120,11 @@ function generateLeaderboard(bot, msg){
 module.exports = {
     name: "leaderboard",
     description: "this command generates a leaderboard of the current users with points",
-    execute(bot, msg) {
-        generateLeaderboard(bot, msg);
+    execute(bot, msg, args) {
+        let maxRows = 10;
+        if (!isNaN(args[0])){
+            maxRows = args[0];
+        }
+        generateLeaderboard(bot, msg, maxRows);
     },
 };
