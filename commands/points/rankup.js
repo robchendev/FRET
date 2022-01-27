931998d8a25278260cec8c82605f8ebef6e32221
Taco <65432314+tradinglounge@users.noreply.github.com>
2022-01-26 21:37:26 -0600

    Moved from static secrets to encapsulated.

--- commands/points/rankup.js
+++ commands/points/rankup.js
@@ -2,10 +2,8 @@ const Discord = require("discord.js");
 const mongoose = require("mongoose");
 const configHandler = require(`../../handlers/configurationHandler.js`);
 configHandler.initialize();
-
-const secrets = require(`../../secrets.json`);
 const pointsAdd = require("../../models/addPoints.js");
-mongoose.connect(secrets.Mongo, {
+mongoose.connect(configHandler.secrets.Mongo, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
 });
